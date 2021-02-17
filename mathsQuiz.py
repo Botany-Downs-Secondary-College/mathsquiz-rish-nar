6c909062a03f4c76d6f22d9acc213d7a7287dc05
rish-nar <62084259+rish-nar@users.noreply.github.com>
2021-02-17 18:24:02 +1300

    Add files via upload
    Difficulty levels added

--- mathsQuiz.py
+++ mathsQuiz.py
@@ -22,6 +22,31 @@ class maths:
         self.HomeButton = ttk.Button(self.Questions, text = "home", command = Show_Welcome)
         self.HomeButton.grid(row=8, column=1)
 
+        self.NameLabel = Label(self.Welcome, text = "name", anchor = W, fg = "black", width = 10, padx= 30, pady = 10)
+        self.NameLabel.grid(row=2, column=0)
+
+        self.AgeLabel = Label(self.Welcome, text = "age", anchor=W, fg = "black", width = 10, padx= 30, pady = 10)
+        self.AgeLebel.grid(row=3, column=0)
+
+        self.NameEntry=ttk.Entry(self.Welcome, width = 20)
+        self.NameEntry.grid(row=3, column = 1, columnspan =2)
+
+        self.AgeEntry=ttk.Entry(self.Welcome, width = 20)
+        self.AgeEntry.grid(row=3, column = 1)
+
+        self.DifficultyLabel = Label(self.Welcome, text = "Choose difficulity level", anchor = W, fg = "black", width = 10, padx = 30, pady = 10)
+        self.DifficultyLabel.grid(row=4, column=0)
+
+        self.difficulty = ["Easy", "Medium", "Hard", "Super Hard"]
+        self.diff_lvl = StringVar()
+        self.diff_level.set(0)
+        self.diff_btns = []
+
+        for i in range(len(self.difficulty)):
+            rb = Radiobutton(self.Welcome, variable = self.diff_lvl, value = 1, text = selff.difficulty[i], anchor = W, padx = 50, width = "5", height = "2")
+            self.diff_btns.append(rb)
+            rb.grid(row= i+5, column = 0, sticky = W)
+
 def Show_Welcome(self):
     self.Questions.grid_remove()
     self.Welcome.grid()
@@ -30,8 +55,10 @@ def Show_Questions(self):
     self.Welcome.grid_remove()
     self.Questions.grid()
 
+
 if __name__ == "__main__":
     root= Tk()
-    frames = maths()
+    frames = maths(root)
+    
     root.title("Quiz")
     root.mainloop()
